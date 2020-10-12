5cc0b35ccf5d038f2eee202ec8ad5288772a607d
Jan Kimr <jan.kimr@student.gyarab.cz>
2020-10-12 09:11:26 +0200

    heroku next commit

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular-node-express'));
+app.use(express.static(__dirname + '/dist/equation-editor'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/angular-node-express/index.html'));
+res.sendFile(path.join(__dirname+'/dist/equation-editor/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
